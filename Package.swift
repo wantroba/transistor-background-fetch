592e5dcdf0e01a7bc7f0ebad033a32651a85fdbc
ewerton.wantroba <wantr0ba@hotmail.com>
2026-04-20 18:53:15 -0300

    package swift adjusted.

--- Package.swift
+++ Package.swift
@@ -1,14 +1,20 @@
 // swift-tools-version: 5.9
 import PackageDescription
+
 let package = Package(
     name: "TSBackgroundFetch",
     platforms: [.iOS(.v12)],
-    products: [.library(name: "TSBackgroundFetch", targets: ["TSBackgroundFetch"])],
-    targets: [
-        .binaryTarget(
+    products: [
+        .library(
             name: "TSBackgroundFetch",
-            url: "https://github.com/transistorsoft/transistor-background-fetch/releases/download/4.1.1/TSBackgroundFetch.xcframework.zip",
-            checksum: "05702e657d4f307082ade707d8d0ae1eda55e112f42bf38febcb0230034c4860"
+            targets: ["TSBackgroundFetch"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "TSBackgroundFetch",
+            path: "ios/TSBackgroundFetch",
+            publicHeadersPath: "."
         )
     ]
 )
